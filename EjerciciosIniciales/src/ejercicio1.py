dd665a28d35991bd0c84448dd36dcfc4a386a763
Gasparpdd <gasparzurea1604@gmail.com>
2020-04-05 13:53:31 +0200

    Mejorado ejercicio 1

--- EjerciciosIniciales/src/ejercicio1.py
+++ EjerciciosIniciales/src/ejercicio1.py
@@ -1,9 +1,10 @@
 print("Introduce dos números.")
-n1= int(input())
-n2= int(input())
+n1 = int(input())
+n2 = int(input())
 suma = n1 + n2
 resta = n1 - n2
 mult = n1 * n2
 divEntera = n1 // n2
 div = n1 / n2
-print("Suma: {}, Resta: {}, Multiplicación: {}, División entera: {}, División: {}".format(suma, resta, mult, divEntera, div))
+print("Suma: {}, Resta: {}, Multiplicación: {}, División entera: {}, División: {}".format(suma, resta, mult, divEntera,
+                                                                                          div))
